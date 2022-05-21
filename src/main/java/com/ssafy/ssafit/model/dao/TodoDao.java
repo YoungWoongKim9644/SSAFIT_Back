42ba22788e943bb290629c246d6a8fa7ee76a300
ssafypark <pbsu1213@hanmail.net>
2022-05-22 05:31:08 +0900

    Revert "Todo!"
    This reverts commit f57cb0d84468f169135449e21c8bec40148403c7.

--- src/main/java/com/ssafy/ssafit/model/dao/TodoDao.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ssafit.model.dao;
-
-import java.util.List;
-
-import com.ssafy.ssafit.model.dto.Todo;
-
-public interface TodoDao {
-	List<Todo> todoList(String id);
-} 
--- src/main/java/com/ssafy/ssafit/model/service/TodoService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ssafit.model.service;
-
-import java.util.List;
-
-import com.ssafy.ssafit.model.dto.Todo;
-
-public interface TodoService {
-	List<Todo> getAction(String id);
-}
